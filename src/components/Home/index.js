cca8aef07c05439922eb8e8378997a8dcd30d56b
Bruno Patrício <bruno.patricio3@hotmail.com>
2021-12-02 11:30:58 +0000

    Minor copy update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ function Home() {
     <>
       <div>
         <p>
-          This website was inspired by the original <a target="_blank" href="https://adventofcode.com/" className="link">Advent of code</a> website, created by  <a target="_blank" href="https://adventofcode.com/2021/about" className="link">Eric Wastl</a>. It's main purpose is to index the various solutions for the various challenges of the 2021 Advent of Code's calendar.  
+          This website was inspired by the original <a target="_blank" href="https://adventofcode.com/" className="link">Advent of code</a> website, created by  <a target="_blank" href="https://adventofcode.com/2021/about" className="link">Eric Wastl</a>. It's main purpose is to index the solutions for the various challenges of the 2021 Advent of Code's calendar.  
         </p>
         <p>
           The reason this website was structured and stylized similar to Eric's Advent of Code webiste, was to maintain the consistency, aspect and spirit of the original and, it is, in no way, an attempt to steal his <span className="text-highlight">incredible work</span> done over the past years.  
