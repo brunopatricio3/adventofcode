dca9eea8d2650e19f3ab3e49aca2c22e1e15897d
Bruno Patrício <bruno.patricio3@hotmail.com>
2021-12-03 20:21:06 +0000

    Implemented Submarine Adventure

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
-import Home from '../components/Home'
-import About from '../components/About'
+import Home from './subpages/Home'
+import Tales from './subpages/Tales'
+import About from './subpages/About'
 import '../styles/main.scss';
 
 
 function Application() {
-  const [selectedPage, setSelectedPage] = useState("Home");
+  const [selectedPage, setSelectedPage] = useState("Tales");
 
   return (
     <div className="appWrapper">
       <div className="mainWrapper">
         <h1>Advent of Code - 2021 Challenge</h1>
         <div className="nav margin-bottom-m">
-          {/* <a onClick={()=> setSelectedPage("Home")} className="nav-item">[Home]</a>
-          <a onClick={()=> setSelectedPage("About")} className="nav-item">[About]</a> */}
+          <a onClick={()=> setSelectedPage("Home")} className="nav-item">[Home]</a>
+          <a onClick={()=> setSelectedPage("Tales")} className="nav-item">[Submarine Adventure]</a>
+          {/* <a onClick={()=> setSelectedPage("About")} className="nav-item">[About]</a> */}
           <a target="_blank" href="https://adventofcode.com/" className="nav-item">[Visit Advent of Code]</a>
         </div>
         {
           selectedPage === "Home" ?
           <Home /> : 
+          selectedPage === "Tales" ?
+          <Tales /> : 
           selectedPage === "About" ?
           <About /> : 
           <div></div>
---
+++ src/pages/subpages/Home/index.js
@@ -0,0 +1,33 @@
+import '../../../styles/main.scss';
+
+function Home() {
+  return (
+    <>
+      <div className="section-home">
+        <p>
+          This website was inspired by the original <a target="_blank" href="https://adventofcode.com/" className="link">Advent of code</a> website, created by  <a target="_blank" href="https://adventofcode.com/2021/about" className="link">Eric Wastl</a>. It's main purpose is to index the solutions for the various challenges of the 2021 Advent of Code's calendar.  
+        </p>
+        <p>
+          The reason this website was structured and stylized similar to Eric's Advent of Code webiste, was to maintain the consistency, aspect and spirit of the original and, it is, in no way, an attempt to steal his <span className="text-highlight-white">incredible work</span> done over the past years.  
+        </p>
+        <p>
+          Below, you can find this year's calendar and respective solutions in Javascript:  
+        </p>
+      </div>
+      <div className="calendar">
+        <h2>Calendar</h2>
+        <p>
+          <a className="calendar-days" target="_blank" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/1/index.js"><span>Day 1) Sonar Sweep</span> <span className="award-gold">**</span></a>
+        </p>
+        <p>
+          <a className="calendar-days" target="_blank" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/2/index.js"><span>Day 2) Dive!</span> <span className="award-gold">**</span></a>
+        </p>
+        <p>
+          <a className="calendar-days" target="_blank" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/3/index.js"><span>Day 3) Binary Diagnostic</span> <span className="award-gold">**</span></a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
