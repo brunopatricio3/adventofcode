99d154dfd21dc089789523b0b3fc35f6ca0dfed7
Bruno Patrício <bruno.patricio3@hotmail.com>
2021-12-07 18:41:38 +0000

    Added day 7 to Home

--- src/pages/subpages/Home/index.js
+++ src/pages/subpages/Home/index.js
@@ -34,6 +34,9 @@ function Home() {
         <p>
           <a className="calendar-days" target="_blank" rel="noreferrer" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/6/index.js"><span>Day 6) Lanternfish</span> <span className="award-gold">**</span></a>
         </p>
+        <p>
+          <a className="calendar-days" target="_blank" rel="noreferrer" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/7/index.js"><span>Day 7) The Treachery of Whales</span> <span className="award-gold">**</span></a>
+        </p>
       </div>
     </>
   );
