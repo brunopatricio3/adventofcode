76aff12c567f85ee3c4c78a97c193c53d82398ce
Bruno Patrício <bruno.patricio3@hotmail.com>
2021-12-09 00:42:44 +0000

    Day 8 changes

--- src/pages/subpages/Home/index.js
+++ src/pages/subpages/Home/index.js
@@ -38,7 +38,7 @@ function Home() {
           <a className="calendar-days" target="_blank" rel="noreferrer" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/7/index.js"><span>Day 7) The Treachery of Whales</span> <span className="award-gold">**</span></a>
         </p>
         <p>
-         <span className="text-noncomplete">Day 8) Seven Segment Search</span>
+          <a className="calendar-days" target="_blank" rel="noreferrer" href="https://github.com/brunopatricio3/adventofcode/blob/main/2021/8/index.js"><span>Day 8) Seven Segment Search</span> <span className="award-silver">*</span></a>
         </p>
       </div>
     </>
